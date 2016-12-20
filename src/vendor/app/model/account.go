6da526a33fc9856c490f9716b1196a412745361f
vorapoj <vorapoj@tnbdsys.com>
2016-12-20 18:06:42 +0700

    Commit Account and Bank Model

---
+++ src/vendor/app/model/account.go
@@ -0,0 +1,31 @@
+package model
+
+import (
+	"app/shared/database"
+)
+
+type Account struct {
+	AccountNumber 		int64		`db:"cid"`
+	ACCNAME 			string 		`db:"accname"`
+	BAL					float64 	`db:"bal"`
+	BALAVL 				float64 	`db:"balavl"`
+	PROVINCE 			int64 		`db:"province"`
+}
+
+func GetAccount(accountNumber int) (Account, error){
+	
+	var err error
+
+	result := Account{}
+
+	switch database.ReadConfig().Type {
+		case database.TypeMySQL:
+			err = database.SQL.Get(&result, "select cid, accname, bal, balavl, province from ACN where cid = ?", accountNumber)
+		case database.TypeMongoDB:
+		case database.TypeBolt:
+		default:
+			err = ErrCode
+	}
+
+	return result, standardizeError(err)
+}
---
+++ src/vendor/app/model/bank.go
@@ -0,0 +1,29 @@
+package model
+
+import (
+	"app/shared/database"
+)
+
+type Bank struct {
+	BKCD 		int16		`db:"bkcd"`
+	NAME 		string 		`db:"name"`
+	ENAME		string 		`db:"ename"`
+}
+
+func GetBanks() ([]Bank, error){
+	
+	var err error
+
+	var result []Bank
+
+	switch database.ReadConfig().Type {
+		case database.TypeMySQL:
+			err = database.SQL.Select(&result, "select bkcd, name, ename from ZUTBLBK")
+		case database.TypeMongoDB:
+		case database.TypeBolt:
+		default:
+			err = ErrCode
+	}
+
+	return result, standardizeError(err)
+}
