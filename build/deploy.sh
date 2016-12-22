13f9cfbdfe06864c50958213b33f55f2d358644b
trek <trek@trek>
2016-12-22 16:06:52 +0700

    update deploy.sh v2

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-chmod +x ./build/env.sh
-chmod +x ./build/build.sh
-
-./build/env.sh
-
-rm -rf ./bin/template
-cp -r ./template ./bin
-
-./build/build.sh
-
-
-PROCESSNO=`ps -ef | grep -v grep | grep ./app | awk '{print $2}'`
-kill -9 ${PROCESSNO}
-
-cd ./bin
-nohup ./app > nohup.out 2>&1 &
+export PATH="$PATH:$PWD/bin"
+cd robot
+robot -v URL:http://localhost:8888/somkiatbank/account/ .
