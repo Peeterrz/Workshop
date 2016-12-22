dfc7e0209c8f4394c628dc297bc219d4dc3dfec8
trek <trek@trek>
2016-12-22 11:50:52 +0700

    add script build for jenkins

---
+++ build/build.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export PATH="$PATH:$PWD/bin"
+export GOPATH="$PWD"
+
+export http_proxy=http://172.16.222.3:9980/
+export https_proxy=http://172.16.222.3:9980/
+export socket_proxy=http://172.16.222.3:9980/
+
+go get -v ./...
+go get -v ./...
+go get -v ./...
+
+# Run tests (JUnit plugin)
+rm -f test.out
+touch test.out
+echo "mode: set" > coverage.out
+for pkg in $(go list ./...);
+do
+    #if [[ $pkg != *"vendor"* ]]; then
+      echo "testing... ${pkg}"
+      go test -v -coverprofile=tmp.out ${pkg} >> test.out
+      if [ -f tmp.out ]; then
+         cat tmp.out | grep -v "mode: set" >> coverage.out
+      fi
+    #fi  
+done
+rm -f ./tmp.out
+cat test.out | go2xunit -output tests.xml
+
+# Generate coverage reports (Cobertura plugin)
+gocov convert coverage.out | gocov-xml > cobertura-coverage.xml
+gocov convert coverage.out | gocov-html > cobertura-coverage.html
+
+# Run lint tools (Compiler warning plugin)
+golint ./... > lint.txt
+
+# Run vet tools (Compiler warning plugin)
+go vet ./... > vet.txt
+
+
+# Build application
+go build ./...
+
+#go install -v -gcflags "-N -l" ./...
