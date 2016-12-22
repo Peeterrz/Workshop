cbd6aaa2720e39753436873e6bdf32e0c176d0d4
9aieng(yosaphol.ji) <yosaphol@hotmail.com>
2016-12-22 10:54:03 +0700

    PBI2.1, 2.2 Transfer then verify is done

--- src/vendor/app/controller/transfer.go
+++ src/vendor/app/controller/transfer.go
@@ -4,6 +4,7 @@ import (
 	//"fmt"
 	"log"
 	"net/http"
+	"strconv"
 	
 	"app/shared/view"
 	"app/model"
@@ -29,34 +30,65 @@ func TransferGET(w http.ResponseWriter, r *http.Request) {
 	v.Name = "transfer/form"
 	v.Vars["from_acc_no"] = from_acc_no
 	v.Vars["banks"] = banks
-	
-	// Refill any form fields
-	//view.Repopulate([]string{"email"}, r.Form, v.Vars)
 	v.Render(w)
 }
 
-func TransferPOST(w http.ResponseWriter, r *http.Request) {
-	log.Println("*** TransferPOST")
-	// Validate with required fields
-	/*if validate, missingField := view.Validate(r, []string{"note"}); !validate {
-		sess.AddFlash(view.Flash{"Field missing: " + missingField, view.FlashError})
-		sess.Save(r, w)
-		NotepadCreateGET(w, r)
-		return
-	}*/
+func Transfer_Verify(w http.ResponseWriter, r *http.Request) {
+	accfromInput := r.FormValue("#from_acc_no")
+	acctoInput := r.FormValue("to_acc_no")
+	bankInput := r.FormValue("bank_code")
+	trnamtInput := r.FormValue("tamt")
 	
-	// Get form values
-	from_acc_no := r.FormValue("#from_acc_no")
-	to_acc_no := r.FormValue("to_acc_no")
-	bank_code := r.FormValue("bank_code")
-	tamt := r.FormValue("tamt")
+	accfrom, err := strconv.Atoi(accfromInput)
+	accto, err := strconv.Atoi(acctoInput)
+	bankCode, err := strconv.Atoi(bankInput)
+
+	accountFromObj, err := model.AccountByAccountNo(accfrom)
+	if err != nil {
+		panic(err)
+	}
 	
-	log.Println("*** from_acc_no = " + from_acc_no)
-	log.Println("*** to_acc_no = " + to_acc_no)
-	log.Println("*** bank_code = " + bank_code)
-	log.Println("*** tamt = " + tamt)
+	accountToObj, err := model.AccountByAccountNo(accto)
+	if err != nil {
+		panic(err)
+	}
 	
+	bankObj, err := model.GetBanksCode(bankCode)
+	if err != nil {
+		panic(err)
+	}  
+	
+	// Display the view
 	v := view.New(r)
-	v.Name = "transfer/verify"
+	v.Name = "transfer/transfer_verify"
+	v.Vars["from_acc_no"] = accountFromObj.CID
+	v.Vars["from_acc_name"] = accountFromObj.ACCNAME
+	v.Vars["to_acc_no"] = accountToObj.CID
+	v.Vars["to_acc_name"] = accountToObj.ACCNAME
+	v.Vars["bank_code"] = bankObj.BKCD
+	v.Vars["bank_name"] = bankObj.NAME
+	v.Vars["trn_amt"] = trnamtInput
+	v.Vars["fee_amt"] = FeeCal(accountFromObj,accountToObj)
 	v.Render(w)
+}
+
+func FeeCal(accountFromObj model.Account ,accountToObj model.Account)(output float64) {
+	Interregion := Interregion(accountFromObj , accountFromObj)
+
+	output=0.00
+	if (Interregion) && (accountFromObj.TRNO>10) {
+			output=10.00
+	}
+
+	return output
+}
+
+func Interregion(accountFromObj model.Account ,accountToObj model.Account)(output bool) {
+	output=false
+	
+	if (accountFromObj.PROVINCE!=accountToObj.PROVINCE) {
+		output=true
+	}
+
+	return output
 }
