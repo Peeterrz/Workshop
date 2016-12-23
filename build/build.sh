3ad63c8463c96c518293ce815ed61a83ffb9d3b4
trek <trek@trek>
2016-12-23 10:02:48 +0700

    add code coverage.

--- build/build.sh
+++ build/build.sh
@@ -10,6 +10,35 @@ export socket_proxy=http://172.16.222.3:9980/
 go get -v ./...
 go get -v ./...
 
+# Run tests (JUnit plugin)
+rm -f test.out
+touch tmp.out
+echo "mode: set" > coverage.out
+for pkg in $(go list ./...);
+do
+    if [[ $pkg != *"github.com"* ]]; then
+      echo "testing... ${pkg}"
+      go test -v -coverprofile=tmp.out ${pkg} >> test.out
+      if [ -f tmp.out ]; then
+         cat tmp.out | grep -v "mode: set" >> coverage.out
+      fi
+    fi  
+done
+
+#go test vendor/app/utilities -coverprofile=tmp.out ${pkg} >> test.out
+rm -f ./tmp.out
+cat test.out | go2xunit -output tests.xml
+
+# Generate coverage reports (Cobertura plugin)
+gocov convert coverage.out | gocov-xml > cobertura-coverage.xml
+gocov convert coverage.out | gocov-html > cobertura-coverage.html
+
+# Run lint tools (Compiler warning plugin)
+golint ./... > lint.txt
+
+# Run vet tools (Compiler warning plugin)
+go vet ./... > vet.txt
+
 # Build application
 go build ./...
 
